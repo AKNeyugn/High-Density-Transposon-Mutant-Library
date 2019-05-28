57fd494e8f1887a94e63d0b413e73e29298d07e0
AKNeyugn <anhkhoi.nguyenhoang@gmail.com>
2019-05-28 13:48:20 -0500

    process_mutants function complete

--- MutantSearcher.py
+++ MutantSearcher.py
@@ -3,9 +3,10 @@ import numpy as np
 import pandas as pd
 
 
-gene_mutant_dict = {}
 processed_mutant_library = "ProcessedMutantLibrary.xlsx"
 output = "EssentialGenesMutants.xlsx"
+filter_tag = "Locus tag"
+
 
 def process_mutants(mutant_library_file):
     '''
@@ -17,15 +18,30 @@ def process_mutants(mutant_library_file):
     '''
 
     with pd.ExcelFile(mutant_library_file) as xls:
-        df = pd.read_excel(xls, "Sheet1", keep_default_na=False, na_values=[""])
-        prev_cog = u""
-        prev_locus_tag = u""
-        for index, row in df.iterrows():
-            if (row['CDS strand'] != "N/A"):
-                if (row['COG'] != unicode(prev_cog) and row['Locus tag'] != unicode(prev_locus_tag)):
-                    print(row['COG'], row['Locus tag'])
-                    prev_cog = row['COG']
-                    prev_locus_tag = row['Locus tag']
+        df_unprocessed = pd.read_excel(xls, "Sheet1", keep_default_na=False, na_values=[""])
+        column = df_unprocessed.columns
+        df_processed = pd.DataFrame(columns=column)
+        prev_row = set()
+
+        for index, row in df_unprocessed.iterrows():
+            # Filter N/A CDS strand and strands with already seen Locus tag
+            if (row["CDS strand"] != "N/A" and row[filter_tag] not in prev_row):
+                # Increase number of columns for mutants with operons
+                new_column = column
+                for j in range(len(row) - len(column)):
+                    new_column.append(j)
+                new_df = pd.DataFrame(columns=new_column)
+
+                # Copy mutant data into new data frame
+                for k in range(len(row)):
+                    df_index = new_df.columns[k]
+                    new_df.at[0, df_index] = row[k]
+                df_processed = df_processed.append(new_df, sort=False)
+
+                # Remember previous row to reduce redundancies
+                prev_row.add(row[filter_tag])
+
+        df_processed.to_excel(processed_mutant_library, index=False)
 
 
 def search_mutants(gene_library_file):

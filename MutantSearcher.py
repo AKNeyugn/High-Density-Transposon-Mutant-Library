f532033a5eebb1abb9c215db77699e070fa3b06d
AKNeyugn <anhkhoi.nguyenhoang@gmail.com>
2019-05-28 14:16:08 -0500

    added cmd progress output for process_mutants

--- MutantSearcher.py
+++ MutantSearcher.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 processed_mutant_library = "ProcessedMutantLibrary.xlsx"
 output = "EssentialGenesMutants.xlsx"
-filter_tag = "Locus tag"
+filter_tag = "Locus TAG"
 
 
 def process_mutants(mutant_library_file):
@@ -16,16 +16,21 @@ def process_mutants(mutant_library_file):
     Args:
         mutant_library_file (string): File name of HDTM library
     '''
-
+    
+    sys.stdout.write("Processing mutant library... \n")
     with pd.ExcelFile(mutant_library_file) as xls:
-        df_unprocessed = pd.read_excel(xls, "Sheet1", keep_default_na=False, na_values=[""])
+        df_unprocessed = pd.read_excel(xls, sheet_name=0, keep_default_na=False, na_values=[""])
         column = df_unprocessed.columns
         df_processed = pd.DataFrame(columns=column)
         prev_row = set()
+        num_mutant_processed = 0
 
         for index, row in df_unprocessed.iterrows():
+            num_mutant_processed += 1
+
             # Filter N/A CDS strand and strands with already seen Locus tag
             if (row["CDS strand"] != "N/A" and row[filter_tag] not in prev_row):
+
                 # Increase number of columns for mutants with operons
                 new_column = column
                 for j in range(len(row) - len(column)):
@@ -40,6 +45,9 @@ def process_mutants(mutant_library_file):
 
                 # Remember previous row to reduce redundancies
                 prev_row.add(row[filter_tag])
+
+            sys.stdout.write("Processed %d mutants \r" % (num_mutant_processed))
+            sys.stdout.flush()
 
         df_processed.to_excel(processed_mutant_library, index=False)
 
@@ -60,7 +68,7 @@ def main(argv=None):
     process_mutants(mutant_library_file)
     search_mutants(gene_library_file)
 
-    print("Done")
+    sys.stdout.write("\nDone")
     return
 
 if __name__ == "__main__":
